e0e7c1fa8637568b6d90f1c14f613f7c484305e2
BayuDC <dev.bayudc@gmail.com>
2022-06-25 20:41:05 +0700

    chore: download picture file

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const { get } = require('https');
 const { send } = require('micro');
 const query = require('micro-query');
 const { ObjectId } = require('mongodb');
@@ -8,13 +9,26 @@ const db = require('./db');
  * @param {import('http').ServerResponse} res
  */
 module.exports = async (req, res) => {
+    // get query
     const { id } = query(req);
-
     if (!id || !ObjectId.isValid(id)) return send(res, 400);
 
+    // fetch data
     const picture = await db.collection('pictures').findOne({ _id: new ObjectId(id) }, { projection: { url: 1 } });
-
     if (!picture) return send(res, 404);
 
-    return { picture };
+    // download file
+    const stream = await new Promise(resolve => {
+        get('https://cdn.discordapp.com/attachments' + picture.url, res => {
+            resolve(res.statusCode == 200 ? res : undefined);
+        });
+    });
+
+    // TODO process file
+
+    // send file
+    res.setHeader('Content-Type', 'image/jpg');
+    res.setHeader('Content-Disposition', `filename="${id}.jpg"`);
+
+    return stream;
 };
