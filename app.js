f88fdf27d581672b8817e73d67a49d4b348aee8f
BayuDC <dev.bayudc@gmail.com>
2022-06-25 19:51:57 +0700

    chore: connect to database

--- app.js
+++ app.js
@@ -1,17 +1,9 @@
-const fs = require('fs');
-const { send } = require('micro');
+const db = require('./db');
 
 /**
  * @param {import('http').IncomingMessage} req
  * @param {import('http').ServerResponse} res
  */
 module.exports = async (req, res) => {
-    const id = '62b6cd35767a5ef421e8d6e4';
-    const path = './data/' + id;
-    const file = fs.createReadStream(path);
-
-    res.setHeader('Content-Type', 'image/jpg');
-    res.setHeader('Content-Disposition', `filename="${id}.jpg"`);
-
-    return file;
+    return 'Hello World';
 };
