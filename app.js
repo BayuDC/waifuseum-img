fcbab8da28a06db203389f34492ebc1e3fd79769
BayuDC <dev.bayudc@gmail.com>
2022-06-25 19:01:01 +0700

    feat: send picture file as response

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
+const fs = require('fs');
+const { send } = require('micro');
+
+/**
+ * @param {import('http').IncomingMessage} req
+ * @param {import('http').ServerResponse} res
+ */
 module.exports = async (req, res) => {
-    return 'Hello World!';
+    const id = '62b6cd35767a5ef421e8d6e4';
+    const path = './data/' + id;
+    const file = fs.createReadStream(path);
+
+    res.setHeader('Content-Type', 'image/jpg');
+    res.setHeader('Content-Disposition', `filename="${id}.jpg"`);
+
+    return file;
 };
