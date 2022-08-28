c07a0c63a55d35b25b5f5b81343a0300e9d16cda
BayuDC <dev.bayudc@gmail.com>
2022-08-28 17:15:21 +0700

    chore: redis is too overkill

--- app.js
+++ app.js
@@ -4,18 +4,18 @@ const sharp = require('sharp');
 const { get } = require('https');
 const { nanoid } = require('nanoid');
 const { ObjectId } = require('mongodb');
-const { mongo, redis } = require('./db');
+const db = require('./db');
 
 const app = new Koa();
 const port = process.env.PORT || 3000;
 
-app.context.mongo = mongo;
-app.context.redis = redis;
+app.context.db = db;
+app.context.caches = new Map();
 
 app.use(async (ctx, next) => {
     const { id, size } = ctx.query;
 
-    const cache = await ctx.redis.get(`${id}:${size}`);
+    const cache = ctx.caches.get(`${id}-${size}`);
     if (!cache) {
         if (!id || !size) ctx.throw(418);
         if (!size.match(/^(thumbnail|minimal|standard|original)$/)) {
@@ -31,7 +31,7 @@ app.use(async (ctx, next) => {
     const { id } = ctx.query;
     if (!ObjectId.isValid(id)) ctx.throw(400);
 
-    const picture = await ctx.mongo.collection('pictures').findOne(
+    const picture = await ctx.db.collection('pictures').findOne(
         { _id: new ObjectId(id) },
         { projection: { url: 1, width: 1, height: 1 } }
         //
@@ -81,7 +81,7 @@ app.use(async (ctx, next) => {
     const { picture } = ctx.state;
 
     try {
-        const name = `${picture._id}:${size}`;
+        const name = `${picture._id}-${size}`;
         let path = `./data/${name}.`;
 
         if (size == 'original') {
@@ -121,11 +121,11 @@ app.use(async (ctx, next) => {
 
     await next();
 });
+
 app.use(ctx => {
     const { picture } = ctx.state;
 
-    ctx.redis.set(picture.name, picture.path);
-    ctx.redis.set(picture.name + '*', 1, { EX: 600 });
+    ctx.caches.set(picture.name, picture.path);
     ctx.body = fs.createReadStream(picture.path);
     ctx.attachment(picture.path, { type: 'inline' });
 });
--- db.js
+++ db.js
@@ -1,34 +1,12 @@
-const fs = require('fs/promises');
 const { MongoClient } = require('mongodb');
-const { createClient } = require('redis');
 
 const dbUri = process.env.DB_URI || 'mongodb://127.0.0.1:27017';
 const dbName = process.env.DB_NAME || 'waifuseum';
 
 const mongo = new MongoClient(dbUri);
-const redis = createClient({
-    url: process.env.REDIS,
-});
 
 mongo.connect().then(() => {
-    console.log('Connected to mongo database');
-});
-redis.connect().then(() => {
-    console.log('Connected to redis database');
+    console.log('Connected to database');
 });
 
-module.exports = {
-    mongo: mongo.db(dbName),
-    redis,
-};
-
-setInterval(async () => {
-    (await redis.keys('*[^*]')).forEach(async key => {
-        const life = await redis.get(key + '*');
-        if (!life) {
-            const cache = await redis.get(key);
-            await fs.unlink(cache);
-            await redis.del(key);
-        }
-    });
-}, 600 * 1000);
+module.exports = mongo.db(dbName);
